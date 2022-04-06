ed8bd78b1449e84b47c9366f955b09317609922b
Arbias <arbias@justt.me>
2022-04-06 13:51:58 +0200

    Initial commit

---
+++ src/components/atoms/input/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const InputComponent = ({name, value, onChange }) => {
+  return (
+    <div>InputComponent</div>
+  )
+}
+
+InputComponent.propTypes = {}
+
+export default InputComponent
